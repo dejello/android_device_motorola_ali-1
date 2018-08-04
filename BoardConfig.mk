a8aa3b9597744fc09806268b578d2a306e509f4e
rahulsnair <rahulsnair30@gmail.com>
2018-08-04 21:33:23 +0000

    ali: add wifi flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,5 +190,18 @@ include device/qcom/sepolicy/sepolicy.mk
 TARGET_INIT_VENDOR_LIB := libinit_ali
 TARGET_RECOVERY_DEVICE_MODULES := libinit_ali
 
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
+BOARD_WLAN_DEVICE                := qcwcn
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_P2P          := "p2p"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
 # inherit from the proprietary version
 -include vendor/motorola/ali/BoardConfigVendor.mk
